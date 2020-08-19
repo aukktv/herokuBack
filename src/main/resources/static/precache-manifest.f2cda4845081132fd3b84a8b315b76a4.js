df574475189ba382c462de95ebc31b463f7a7eda
Auksė Kotovičienė <aukse.kotoviciene@gmail.com>
2020-08-19 16:52:10 +0300

    React added to heroku

---
+++ src/main/resources/static/precache-manifest.f2cda4845081132fd3b84a8b315b76a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4b4ee2e719a346ff13e5fec80453868",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f49522dd10a86eb920fa",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ab1617f95246f86a2b91",
+    "url": "/static/css/main.87987d25.chunk.css"
+  },
+  {
+    "revision": "f49522dd10a86eb920fa",
+    "url": "/static/js/2.eaca910b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.eaca910b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab1617f95246f86a2b91",
+    "url": "/static/js/main.b6edbd12.chunk.js"
+  },
+  {
+    "revision": "e4ce0490061b5d4dac80",
+    "url": "/static/js/runtime-main.9cab4020.js"
+  }
+]);
